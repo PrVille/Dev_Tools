ee7b28c6aa29069c3ad44a3327f1603b54fd3ad7
ville <ville.prami@aalto.fi>
2023-03-28 17:13:35 +0300

    1.0.1

--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,12 @@ import { ThemeProvider } from "@mui/material/styles"
 import { BrowserRouter as Router } from "react-router-dom"
 import NotificationProvider from "./providers/NotificationProvider"
 import Notification from "./components/Notification"
-//import { inject } from '@vercel/analytics';
+import { inject } from '@vercel/analytics';
  
 import App from "./App"
 import theme from "./theme"
 
-//inject();
+inject();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Router>
